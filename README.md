6d8a2d0e23f16200786a9b4e5c6fb5ac0b4e1386
Tejas2171 <tejassonawane11102004@gmail.com>
2024-03-07 22:16:10 +0530

    initialise the main file and inputs by command line args

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DSA-Project
+# Version Control System
+
+A local version control for keeping track of progress and the history
+inspired by git
---
+++ code/project.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+    //depending upon these command line arguments we will be deciding operations to be done
+
+    return 0;
+}
