ba3d0440b4e662232038d04e5eea1b985199e2ea
Tejas2171 <tejassonawane11102004@gmail.com>
2024-04-11 10:38:58 +0530

    String implementation of vector using dynamic memory allocation

--- code/Vector.h
+++ code/Vector.h
@@ -1,13 +1,13 @@
 typedef struct {
-    int* array;     
-    size_t size;    
-    size_t capacity; 
+    char** data;
+    size_t size;
+    size_t capacity;
 } Vector;
 
 
 void Init_vector(Vector* vec);
-void Vector_push_back(Vector* vec, int value);
+void Vector_push_back(Vector* vec, const char* value);
 void Vector_pop_back(Vector* vec);
-int Vector_at(Vector* vec, size_t index);
+const char* Vector_at(Vector* vec, size_t index);
 int Vector_isempty(Vector* vec);
 void Vector_free(Vector* vec);
